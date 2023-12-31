906d8fd14d5f4411fe129f33f882a589d25f4e54
Sam Potter <spotter1642@gmail.com>
2023-12-31 13:13:32 -0600

    Add basics of parsing board squares

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,54 @@
+#[derive(Clone, Copy)]
+enum Marking {
+    Red,
+    Green,
+    Blue,
+    Yellow,
+    Orange,
+    Purple
+}
+
+impl TryFrom<&str> for Marking {
+    type Error = ();
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        match value {
+            "r" => Ok(Marking::Red),
+            "g" => Ok(Marking::Green),
+            "b" => Ok(Marking::Blue),
+            "y" => Ok(Marking::Yellow),
+            "o" => Ok(Marking::Orange),
+            "p" => Ok(Marking::Purple),
+            _ => Err(())
+        }
+    }
+}
+
+#[derive(Clone, Copy)]
+struct Square {
+    position: usize,
+    marking: Marking
+}
+
+impl Square {
+    fn new(value: (usize, Marking)) -> Self {
+        let (position, marking) = value;
+        Self { position, marking }
+    }
+}
+
+#[derive(Clone)]
+struct Board {
+    squares: Vec<Square>
+}
 
 fn main() {
-    let board = include_str!("board.txt");
-    for line in board.split(",") {
-       println!("{line}");
-    }
+    let sqaures: Vec<Square> = include_str!("board.txt")
+        .split(",")
+        .enumerate()
+        .map(|(idx, pattern)| Square::new((idx, Marking::try_from(pattern).unwrap())))
+        .collect();
+    let board = Board {
+        squares: sqaures
+    };
 }
